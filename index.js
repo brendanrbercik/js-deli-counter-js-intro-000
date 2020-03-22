084ac6a61390c99fcb1e196bd58ddc8d89cef91b
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-22 19:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine (line) {
 
   for (let i = 0; i < line.length; i++) {
     currentList.push(`${i+=1}: ${line[i]}`);
-    return(`The line is currently: ${currentList}`);
+    return("The line is currently: " + ${currentList});
   }
 
   do {
