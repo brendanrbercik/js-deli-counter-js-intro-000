9bedeb06e87a025e80f4c5fb7d37e4a35c5ba413
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-22 18:59:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ function nowServing (line) {
 
 
 function currentLine (line) {
-  do {
-    return ("The line is currently empty.");
-  } while (line.length === 0);
-
   var currentList = [];
 
   for (let i = 0; i < line.length; i++) {
     currentList.push(`${i+=1}: ${list[i]}`);
     return("The line is currently: ${currentList}");
   }
+
+  do {
+    return ("The line is currently empty.");
+  } while (line.length === 0);
 
   }
